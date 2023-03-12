8569fb6fa5b633cb86e638bf116ecb8bd2e8f94b
Viktoriya Koval <vrkoval@gmail.com>
2023-03-12 20:26:19 +0200

    ``

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,2 @@
 const categoriesCount=document.querySelectorAll('.li.item').length;
-console.log(`Number of categories: '${categoriesCount}');
+console.log(`Number of categories: '${categoriesCount}'`);
