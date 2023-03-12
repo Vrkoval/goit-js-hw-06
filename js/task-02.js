fd4609d9645989eb8098d3ccf5e3294a5aa8b908
Viktoriya Koval <vrkoval@gmail.com>
2023-03-12 21:57:01 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList=document.querySelector('#ingredients');
+console.log(ingredients);
+ingredients.map((ingredient)=>{
+  const ingredientsItem=document.createElement("li");
+  ingredientsItem.classList.add("item");
+  ingredientsList.append(ingredientsItem);
+  ingredientsItem.textContent=ingredient;
+})
