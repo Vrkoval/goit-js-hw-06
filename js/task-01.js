d957d1d0515e74cce04eae4b7f01bb679f100776
Viktoriya Koval <vrkoval@gmail.com>
2023-03-12 21:27:04 +0200

    task-1 with forEach

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,14 @@
-const categoriesCount=document.querySelectorAll('.li.item').length;
-console.log(`Number of categories: '${categoriesCount}'`);
+const categoriesCount=document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoriesCount.length}`);
+console.log("");
+
+// const allItemList = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${allItemList.length}`);
+categoriesCount.forEach((item)=>{
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+// //   пробел
+ console.log("");
+
+
+});
