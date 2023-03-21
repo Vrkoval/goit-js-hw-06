3bb9adf7cb424c06a27e494695ba0041cfa5e176
Viktoriya Koval <vrkoval@gmail.com>
2023-03-21 22:48:32 +0200

    t9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  btn: document.querySelector(".change-color"),
+  bgColor: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+
+const makeBackground = (event) => {
+  const bodyColor = refs.body.style.backgroundColor = getRandomHexColor();
+  refs.bgColor.textContent = bodyColor;
+
+};
+refs.btn.addEventListener("click", makeBackground);
