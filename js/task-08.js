ab9fc356beac4442bb06481494c636d8662683a0
Viktoriya Koval <vrkoval@gmail.com>
2023-03-21 22:44:02 +0200

    t8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formElement = document.querySelector(".login-form");
 
+formElement.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
